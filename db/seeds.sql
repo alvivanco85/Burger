94f6e8414a429c0e3809b1e73944bd6960c926ee
Linky Linkerson <aldovibanco85@gmail.com>
2020-05-06 15:25:47 -0700

    adding all starting files and folders

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (burger_name, devoured) VALUES ("Baconator", true);
+INSERT INTO burger (burger_name, devoured) VALUES ("Veganator", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("LettuceWrap", true);
+INSERT INTO burger (burger_name, devoured) VALUES ("Carnivour", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("DoubleCheese", false);
