0d5ba5a30d6f2130a727913aa038238bbca6282b
Linky Linkerson <aldovibanco85@gmail.com>
2020-05-20 20:51:58 -0700

    adding root

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Alchris.c1!",
+    password: "root",
     database: "burgers_db"
   });
 
